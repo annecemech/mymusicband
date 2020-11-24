20d346b82c37a29fc1d85b31eb83ed0f6ec2e16b
Remi Schwartz <schwartz.remi@gmail.com>
2020-11-24 14:45:30 +0100

    initial text only seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,170 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Start"
+puts "Clean DB"
+Comment.destroy_all
+Recording.destroy_all
+Partition.destroy_all
+Track.destroy_all
+Band.destroy_all
+User.destroy_all
+
+#----------------------------------------------------
+puts "Seeding users..."
+
+user1 = User.new(
+  first_name: "Kurt",
+  last_name: "Cobain",
+  email: "kurt@mymusicband.com",
+  password: "123456",
+  instrument: 1 # guitare?
+)
+user1.save!
+
+user2 = User.new(
+  first_name: "Dave",
+  last_name: "Grohl",
+  email: "dave@mymusicband.com",
+  password: "123456",
+  instrument: 1 # guitare?
+)
+user2.save!
+
+user3 = User.new(
+  first_name: "Nate",
+  last_name: "Mendel",
+  email: "nate@mymusicband.com",
+  password: "123456",
+  instrument: 2 # bass?
+)
+user3.save!
+
+user4 = User.new(
+  first_name: "Taylor",
+  last_name: "Hawkins",
+  email: "taylor@mymusicband.com",
+  password: "123456",
+  instrument: 3 # batterie?
+)
+user4.save!
+
+#--------------------------------------------------------
+puts "Seeding bands"
+
+band1 = Band.new(
+  name: "Scream",
+  description: "Punk hardcore, post-hardcore, art rock",
+  creation_year: "1981",
+)
+band1.save!
+
+band2 = Band.new(
+  name: "Nirvana",
+  description: "Grunge, rock alternatif",
+  creation_year: "1987",
+)
+band2.save!
+
+band3 = Band.new(
+  name: "Foo fighters",
+  description: "Hard rock, post-grunge, alternative rock",
+  creation_year: "1994"
+)
+band3.save!
+
+#--------------------------------------------------------
+puts "Seeding tracks..."
+
+track1 = Track.new(
+  title: "Everlong",
+  style: "Post-grunge",
+  duration: 4*60+10,
+  tempo: 156,
+  pattern: 4 # 4/4
+)
+track1.band = band3
+track1.save!
+
+track2 = Track.new(
+  title: "The Pretender",
+  style: "Post-grunge",
+  duration: 4*60+27,
+  tempo: 172,
+  pattern: 4 # 4/4
+)
+track2.band = band3
+track2.save!
+
+track3 = Track.new(
+  title: "Best of You",
+  style: "Post-grunge",
+  duration: 4*60+16,
+  tempo: 65,
+  pattern: 4 # 4/4
+)
+track3.band = band3
+track3.save!
+
+#--------------------------------------------------------
+puts "Seeding partitions"
+
+partition1 = Partition.new(
+  name: "Best drum tab I found so far",
+  instrument: 3 #drums
+)
+partition1.track = track2
+partition1.user = user2
+partition1.save!
+
+partition2 = Partition.new(
+  name: "Guitar tab by Tokyo Hotel",
+  instrument: 1 #guitar
+)
+partition2.track = track2
+partition2.user = user2
+partition2.save!
+
+partition3 = Partition.new(
+  name: "Goes like dom doum dom dom",
+  instrument: 2 #bass
+)
+partition3.track = track2
+partition3.user = user2
+partition3.save!
+
+#--------------------------------------------------------
+puts "Seeding recording..."
+
+recording1 = Recording.new(
+  name: "First try with the drums",
+  is_lead: false
+)
+recording1.partition = partition1
+recording1.user = user4
+recording1.save!
+
+recording2 = Recording.new(
+  name: "Now that's better",
+  is_lead: true
+)
+recording2.partition = partition1
+recording2.user = user4
+recording2.save!
+
+recording3 = Recording.new(
+  name: "Tried a little solo at the end",
+  is_lead: false
+)
+recording3.partition = partition2
+recording3.user = user2
+recording3.save!
+
+#-------------------------------------------------------
+puts "Seeding comments"
+
+comment1 = Comment.new(
+  content: "#guitar_recording Dave, keep to the tab for F#$! sake. Quit the solos!"
+  )
+comment1.user = user3
+comment1.track = track2
+comment1.save!
+
+puts "Finished"
