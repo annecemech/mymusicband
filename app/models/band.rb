60776a8a82dfa2c36d4663015a56ad6184ab4153
AnneCecile Mechin <ac.mechin@gmail.com>
2020-11-24 11:55:05 +0100

    Add validations to models

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,2 +1,8 @@
 class Band < ApplicationRecord
+  has_many :members
+  has_many :users, through: :members
+  has_many :tracks
+
+  validates :name, presence: true
+  validates :description, presence: true
 end
