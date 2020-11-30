0e7c1888d5130846374632219f05fe555ee972d7
Maxime Schneiter <maximeschneiter@gmail.com>
2020-11-30 16:26:18 +0100

    mix buttons

--- app/javascript/components/buttons-music.js
+++ app/javascript/components/buttons-music.js
@@ -2,39 +2,34 @@
 const playcheckbox = () => {
   const buttonPlay = document.querySelector('.btn-play');
   const buttonPause = document.querySelector('.btn-pause');
-  const buttonStop = document.querySelector('.btn-stop');
   const buttonRecord = document.querySelector('.btn-record');
   const checkbox = document.querySelectorAll('.checkrecording');
   const audioarray = [];
 
-  checkbox.forEach(element => {
-    element.addEventListener('change', (event) => {
-      if(element.checked) {
-        audioarray.push(new Audio(element.dataset.recordurl));
-      } else {
-        audioarray.splice(new Audio(element.dataset.recordurl), 1)
-      }
-    });
-  });
 
-  buttonPlay.addEventListener('click', (event) => {
-    audioarray.forEach(element => {
-        element.volume = 0.9;
-        element.play();
+    checkbox.forEach(element => {
+      element.addEventListener('change', (event) => {
+        if(element.checked) {
+          audioarray.push(new Audio(element.dataset.recordurl));
+        } else {
+          audioarray.splice(new Audio(element.dataset.recordurl), 1)
+        }
+      });
     });
-  });
 
-  buttonPause.addEventListener('click', (event) => {
-    audioarray.forEach(element => {
-        element.pause();
+    buttonPlay.addEventListener('click', (event) => {
+      audioarray.forEach(element => {
+          element.volume = 0.9;
+          element.play();
+      });
     });
-  });
 
-  buttonStop.addEventListener('click', (event) => {
-    audioarray.forEach(element => {
-        element.load();
+    buttonPause.addEventListener('click', (event) => {
+      audioarray.forEach(element => {
+          element.pause();
+      });
     });
-  });
+
 }
 
 export {playcheckbox};
---
+++ app/javascript/components/buttons-recordings.js
@@ -0,0 +1,40 @@
+
+const mixRecordings = () => {
+  const playRecording = document.querySelector('.play-recording');
+  const pauseRecording = document.querySelector('.pause-recording');
+  const stopRecording = document.querySelector('.stop-recording');
+  const recordingCheckbox = document.querySelectorAll('recording-checkbox');
+  const recordingsArray = [];
+
+  checkrecording.forEach(element => {
+    element.addEventListener('change', (event) => {
+      if(element.checked) {
+        recordingsArray.push(new Audio(element.dataset.recordurl));
+      } else {
+        recordingsArray.splice(new Audio(element.dataset.recordurl), 1)
+      }
+    });
+  });
+
+  playRecording.addEventListener('click', (event) => {
+    recordingsArray.forEach(element => {
+        element.volume = 0.9;
+        element.play();
+    });
+  });
+
+  pauseRecording.addEventListener('click', (event) => {
+    recordingsArray.forEach(element => {
+        element.pause();
+    });
+  });
+
+  stopRecording.addEventListener('click', (event) => {
+    recordingsArray.forEach(element => {
+        element.load();
+    });
+  });
+
+}
+
+export { mixRecordings };
