8b476f11fe976046a43af7e254492f60a1b910d3
baptiste derenne <derenne.baptiste@gmail.com>
2020-12-01 09:44:30 +0100

    eventlistener with record and stop button

--- app/javascript/components/buttons-music.js
+++ app/javascript/components/buttons-music.js
@@ -1,56 +1,37 @@
-import { initCountdown } from '../components/countdown';
-
 
 const playcheckbox = () => {
-
-  const buttonPlay = document.querySelector('.btn-play');
+  const buttonRecordPlay = document.querySelector('.btn-record');
+  const buttonRecordStop = document.querySelector('.btn-stop');
   const buttonPause = document.querySelector('.btn-pause');
-  const buttonRecord = document.querySelector('.btn-record');
   const checkbox = document.querySelectorAll('.checkrecording');
   const audioarray = [];
 
-  let abortController = null;
-
-  if (buttonRecord)
-  {
-
-    checkbox.forEach(element => {
-      element.addEventListener('change', (event) => {
-        if(element.checked) {
-          audioarray.push(new Audio(element.dataset.recordurl));
-        } else {
-          audioarray.splice(new Audio(element.dataset.recordurl), 1)
-        }
-      });
-    });
-
-    buttonPlay.addEventListener('click', (event) => {
-      audioarray.forEach(element => {
-          element.volume = 0.9;
-          element.play();
-      });
-    });
-
-    buttonPause.addEventListener('click', (event) => {
-      audioarray.forEach(element => {
-          element.pause();
-      });
-
-      // if recording ongoing, we stop the scrolling
-      if ( abortController ) {
-        abortController.abort();
-        abortController = null;
+  checkbox.forEach(element => {
+    element.addEventListener('change', (event) => {
+      if(element.checked) {
+        audioarray.push(new Audio(element.dataset.recordurl));
+      } else {
+        audioarray.splice(new Audio(element.dataset.recordurl), 1)
       }
     });
+  });
 
-    buttonRecord.addEventListener('click', (event) => {
-      abortController = new AbortController();
-      initCountdown(audioarray, abortController.signal);
+  buttonRecordPlay.addEventListener('click', (event) => {
+    audioarray.forEach(element => {
+        element.volume = 0.1;
+        element.play();
     });
+    event.currentTarget.classList.add("button-inactive");
+    buttonRecordStop.classList.remove("button-inactive");
+  });
 
-  }
-
+  buttonRecordStop.addEventListener('click', (event) => {
+    audioarray.forEach(element => {
+        element.pause();
+    });
+    event.currentTarget.classList.add("button-inactive");
+    buttonRecordPlay.classList.remove("button-inactive");
+  });
 }
 
-
-export {playcheckbox };
+export {playcheckbox};
