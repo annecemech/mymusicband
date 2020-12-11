2c2a7e92ffcf890581e1f519739dcafd67e0efbc
Remi Schwartz <schwartz.remi@gmail.com>
2020-12-11 17:16:01 +0100

    modified route, added track#create, and modal

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -9,4 +9,24 @@ class TracksController < ApplicationController
     @partition = Partition.new
     @inspiration = Inspiration.new
   end
+
+  def create
+
+    @track = Track.new(track_params)
+
+    @band = Band.find(params[:band_id])
+    @track.band = @band
+
+    if @track.save
+      redirect_to track_path(@track)
+    else
+      render 'bands/show'
+    end
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:title, :artist, :style, :duration, :tempo, :pattern, :photo)
+  end
 end
