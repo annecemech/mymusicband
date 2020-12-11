dcd3b50a836ecccc6718df99c739e1cd1200634d
Remi Schwartz <schwartz.remi@gmail.com>
2020-12-11 15:31:45 +0100

    route, methods, modal/button added

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -4,4 +4,22 @@ class BandsController < ApplicationController
     @band = Band.find(params[:id])
   end
 
+  def create
+    @band = Band.new(band_params)
+
+    member = Member.new(user: current_user, band: @band)
+
+    if @band.save && member.save
+      redirect_to band_path(@band)
+    else
+      @band = Band.new
+      render 'my/bands/index'
+    end
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:name, :description, :creation_year, :photo)
+  end
 end
