8febccd66c56e7dec560f43f1825ab3531dd9873
Remi Schwartz <schwartz.remi@gmail.com>
2021-02-08 17:04:40 +0100

    I18n config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  def set_locale
+    # if user_signed_in?
+    #   I18n.locale = current_user.language
+    # else
+    # end
+    I18n.locale = params[:lang] || locale_from_header || I18n.default_locale
+  end
+
+  def locale_from_header
+    request.env.fetch('HTTP_ACCEPT_LANGUAGE', '').scan(/[a-z]{2}/).first
   end
 
   protected
--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module Mymusicband
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.available_locales = [:en, :fr]
+    config.i18n.default_locale = :en
+    config.i18n.fallbacks = true
   end
 end
